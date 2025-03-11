3e36402b0196ed6c2b5a6645faee4cf30d6f8bd4
PabloDomiciano <pablo.domiciano2019@gmail.com>
2025-03-11 15:19:21 -0300

    configurando vite

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/barman-gallery-experience/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
