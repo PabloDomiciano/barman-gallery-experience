3166d77c7544d675543dd58bf6c159a434a1483a
Pablo Henrique Lopes Domiciano <105524210+PabloDomiciano@users.noreply.github.com>
2025-03-11 15:46:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/barman-gallery-experience/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
